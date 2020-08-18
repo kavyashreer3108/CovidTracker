6695ee9a1a3aaf4690721ae6b12008289511862b
kavyashreer3108 <kavyashreer3108@gmail.com>
2020-08-18 14:02:59 +0530

    Updates

---
+++ precache-manifest.2429e2681f18ad05d098f7ded9980128.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e76a7bd48a848e8e0dbf5275b371839a",
+    "url": "/Covidtracker/index.html"
+  },
+  {
+    "revision": "c4d6601c46b4781fade0",
+    "url": "/Covidtracker/static/css/main.90ba7ce2.chunk.css"
+  },
+  {
+    "revision": "441d9d88e8a32ba14774",
+    "url": "/Covidtracker/static/js/2.974591b9.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covidtracker/static/js/2.974591b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4d6601c46b4781fade0",
+    "url": "/Covidtracker/static/js/main.1cfd88f5.chunk.js"
+  },
+  {
+    "revision": "4931e72cf485bd8e80b7",
+    "url": "/Covidtracker/static/js/runtime-main.1fb209fb.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covidtracker/static/media/image.d7265326.png"
+  }
+]);
